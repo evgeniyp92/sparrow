2f137ffc3369a9cb89ea4127184c4d321c80eb75
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2023-09-05 10:50:55 +0200

    implement fonts and set colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <div>Sparrow</div>;
+  return (
+    <div className='w-screen h-screen bg-slate-900'>
+      <h1 className='text-white font-b612'>Sparrow</h1>
+      <h3 className='text-green-600 font-b612mono uppercase'>Select a checklist</h3>
+      <ul>
+        <li className='text-green-600 font-b612mono uppercase'>
+          <span className='text-cyan-500'>Boeing 737</span> • Elec Power Up
+        </li>
+      </ul>
+    </div>
+  );
 }
