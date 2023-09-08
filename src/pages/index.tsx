fcdfea3cd2cbc11294533d12a6b44f5f0b3477b2
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2023-09-08 09:28:53 +0200

    add accurate coloring and checklist handling btn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,33 +26,42 @@ export default function Home() {
         </a>
       </div>
       <div id='checklist-specimen' className='font-b612mono uppercase m-4 p-4 w-[50rem]'>
-        <p className='text-center text-lg text-white'>cockpit preparation checklist</p>
-        <div>
-          <div className='flex flex-row justify-between items-end text-white'>
+        <p className='text-center text-lg text-white mb-4'>cockpit preparation checklist</p>
+        <div className='flex flex-col space-y-1'>
+          <div className='flex flex-row justify-between items-end text-cyan-500 border-2 border-cyan-400 px-2'>
+            <span className='text-xl mr-4'>□</span>
             <p>Gear pins/covers</p>
-            <div className='flex-auto border-b-2 h-3 mx-1 mb-1.5 border-dotted' />
-            <p>removed</p>
+            <div className='flex-auto border-b-4 h-3 ml-4 mb-1.5 border-dotted border-cyan-600' />
+            <p>remove</p>
           </div>
-          <div className='flex flex-row justify-between items-end text-white'>
+          <div className='flex flex-row justify-between items-end text-green-400 pl-2'>
+            <span className='text-xl mr-4'>□</span>
             <p>fuel</p>
-            <div className='flex-auto border-b-2 h-3 mx-1 mb-1.5 border-dotted' />
+            <div className='flex-auto border-b-4 h-3 ml-4 mb-1.5 border-dotted border-green-400' />
             <p>_____KG/lb</p>
           </div>
-          <div className='flex flex-row justify-between items-end text-white'>
+          <div className='flex flex-row justify-between items-end text-cyan-500 pl-2'>
+            <span className='text-xl mr-4'>□</span>
             <p>Seat belts</p>
-            <div className='flex-auto border-b-2 h-3 mx-1 mb-1.5 border-dotted' />
+            <div className='flex-auto border-b-4 h-3 ml-4 mb-1.5 border-dotted border-cyan-500' />
             <p>on</p>
           </div>
-          <div className='flex flex-row justify-between items-end text-white'>
+          <div className='flex flex-row justify-between items-end text-cyan-500 pl-2'>
+            <span className='text-xl mr-4'>□</span>
             <p>adirs</p>
-            <div className='flex-auto border-b-2 mx-1 mb-1.5 border-dotted' />
+            <div className='flex-auto border-b-4 ml-4 mb-1.5 border-dotted border-cyan-500' />
             <p>nav</p>
           </div>
-          <div className='flex flex-row justify-between items-end text-white'>
+          <div className='flex flex-row justify-between items-end text-cyan-500 pl-2'>
+            <span className='text-xl mr-4'>□</span>
             <p>baro ref</p>
-            <div className='flex-auto border-b-2 mx-1 mb-1.5 border-dotted' />
+            <div className='flex-auto border-b-4 ml-4 mb-1.5 border-cyan-500 border-dotted' />
             <p>check set and xcheck</p>
           </div>
+        </div>
+        <div className='flex flex-col items-center mt-4'>
+          <p className='uppercase font-b612mono text-white'>checklist complete</p>
+          <button className='m-4 p-4 min-w-fit font-b612mono text-black bg-cyan-500'>Check</button>
         </div>
       </div>
     </div>
